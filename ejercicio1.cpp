aa7cabcebdfa345e217e5707c07d6b65760f786f
Default user <ac.calderonz@uniandes.edu.co>
2019-11-12 21:52:42 +0000

    euler

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -2,50 +2,23 @@
 
 #include <iostream>
 #include <cmath>
-void integralE(float tf,float dt, int w);
-void integralI(float tf,float dt, int w);
-float ynE(float t, float dt, float w);
-float ynI(float t, float dt, float w);
+void integralE(float tf,float dt, float w, float m);
 
 int main(){
-    float w = 2;
-    integralE(4/w, 0.1/w, w);
-    integralI(4/w, 0.1/w, w);
-    
-    integralE(4/w, 0.01/w, w);
-    integralI(4/w, 0.01/w, w); 
-    
-    integralE(4/w, 1/w, w);
-    integralI(4/w, 1/w, w);
-    
+    float w = 100;
+    float m=2;
+    integralE(10, 0.01, w,m);    
     return 0;
 }
 
-float ynE(float t, float dt, float w){
-    float ysig= exp(-w*t) - dt*w*exp(-w*t);
-    return ysig;
-}
-
-float ynI(float t, float dt, float w){
-    float ysig= exp(-w*t) - dt*w*exp(-w*t +1);
-    return ysig;
-}
-
-
-void integralE(float tf,float dt, int w){
+void integralE(float tf,float dt, float w, float m){
     std::cout <<"Integral explicita de Euler"<< std::endl;
+    float x=1;
+    float vE=1;
 
-    for(float t=0; t<tf;t+=dt){
-        float yE= ynE(t, dt,w);   
-        std::cout <<"t= "<< t << " " << "Integral= "<< yE<< std::endl;
-    }
-}
-
-void integralI(float tf,float dt, int w){
-    std::cout <<"Integral implicita"<< std::endl;
-
-    for(float t=0; t<tf;t+=dt){
-        float yI= ynI(t, dt,w);
-        std::cout <<"t= "<< t << " " << "Integral= "<< yI << std::endl;
+    for(float t=0; t<=tf;t+=dt){
+        x-= dt*w/m*x;
+        vE= vE+ dt* vE;
+        std::cout << t << x<< " " << vE<< std::endl;
     }
 }
